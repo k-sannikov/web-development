212d331fcd861a5393a2773273343c9dd29b7668
Sannikov Kirill <kir.sannikow@mail.ru>
2021-04-16 12:09:12 +0300

    ЛР 7

---
+++ lw7/src/common.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/pages/feedbacks_list_page.php';
+require_once __DIR__ . '/pages/main_page.php';
+require_once __DIR__ . '/pages/save_feedback_page.php';
+require_once __DIR__ . '/utils/request.php';
+require_once __DIR__ . '/utils/template.php';
+require_once __DIR__ . '/utils/validation.php';
---
+++ lw7/src/templates/feedbacks.tpl.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../web/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <title>Обратная связь</title>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="navbar-feedback">
+        <div class="navbar__header">Меню</div>
+        <div class="navbar__item navbar__item_close">
+          <a class="navbar__link" href="index.php">
+            Главная
+          </a>
+        </div>
+      </nav>
+    </header>
+    <main class="main-feedback">
+    <section class="bottom-container">
+      <form class="form" method="post">
+        <label class="form__label form__label_required">Email пользователя</label>
+        <input class="form__input" type="email" name="email" value="<? echo $args['email'] ?? ''; ?>" />
+        <? if(isset($args['email_error_msg'])): ?>
+          <p class="form__text_error"><? echo $args['email_error_msg']; ?></p>
+        <? endif; ?>
+        <div class="form__button-box" >
+          <button class="button" type="submit">Отправить</button>
+        </div>
+      </form>
+      <? if(isset($args['name'])):?>
+        <div class="feedback-list">
+          <p class="feedback-list__item"><b>Имя:</b> <? echo $args['name'] ?? ''; ?> </p>
+          <p class="feedback-list__item"><b>E-mail:</b> <? echo $args['email'] ?? ''; ?> </p>
+          <p class="feedback-list__item"><b>Страна:</b> <? echo $args['country'] ?? ''; ?> </p>
+          <p class="feedback-list__item"><b>Пол:</b> <? echo $args['gender'] === 'male' ? 'Мужской' : 'Женский'; ?> </p>
+          <p class="feedback-list__item"><b>Сообщение:</b> <? echo $args['message'] ?? ''; ?> </p>
+        </div>
+      <? endif; ?>
+      </section>
+    </main>
+  </body>
+</html>
